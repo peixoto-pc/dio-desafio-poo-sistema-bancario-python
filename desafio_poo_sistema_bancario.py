05d6bec35ab6815aff273c0e56e97fdc200f8367
peixoto-pc <p.peixoto@gmail.com>
2024-05-18 15:17:05 -0300

    update 2

--- desafio_poo_sistema_bancario.py
+++ desafio_poo_sistema_bancario.py
@@ -1,67 +1,54 @@
 from abc import ABC, abstractmethod
-from typing import List
+
 
 class Transacao(ABC):
     @abstractmethod
     def registrar(self, conta):
         pass
 
+    @property
+    @abstractmethod
+    def valor(self):
+        pass
+
+
 class Deposito(Transacao):
     def __init__(self, valor):
-        self.valor = valor
-
+        self._valor = float(valor)
+    
     def registrar(self, conta):
-        # Implementação para registrar depósito
         pass
+    
+    @property
+    def valor(self):
+        return self.valor
 
 class Saque(Transacao):
     def __init__(self, valor):
-        self.valor = valor
+        self._valor = float(valor)
 
-    def registrar(self, conta):
-        # Implementação para registrar saque
-        pass
+    @property
+    def valor(self):
+        return self.valor
 
 class Historico:
     def __init__(self):
-        self._transacoes = []
-
+        self.transacoes = []
+    
+    @property
+    def transacoes(self):
+        return self._transacoes
+    
     def adicionar_transacao(self, transacao):
-        self._transacoes.append(transacao)
-
-class Conta:
-    def __init__(self, cliente, numero):
-        self.cliente = cliente
-        self.numero = numero
-        self.saldo = 0
-        self.historico = Historico()
-
-    def saldo(self):
-        return self.saldo
-
-    def nova(self, cliente, numero):
-        # Implementação para criar nova conta
-        pass
-
-    def sacar(self, valor):
-        # Implementação para realizar saque
-        pass
-
-    def depositar(self, valor):
-        # Implementação para realizar depósito
-        pass
-
-class ContaCorrente(Conta):
-    def __init__(self, cliente, numero, limite, limite_saques):
-        super().__init__(cliente, numero)
-        self.limite = limite
-        self.limite_saques = limite_saques
+        self.transacoes.append(transacao)
 
 class Cliente:
     def __init__(self, endereco):
         self.endereco = endereco
         self.contas = []
 
+
     def realizar_transacao(self, conta, transacao):
-        # Implementação para realizar transação
-        pass
+        transacao.registrar(conta)
+
+dep = Deposito(100)
